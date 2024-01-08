d939acd8cde7221aeaf92bdb8e37787df78f505c
LaTiffiny Barrow <latiffiny@gmail.com>
2024-01-08 16:58:42 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+
+print('Hi, ' + name)
+input("Would you like to play 3 or 6 holes today? ")
+
+input("How many putts for hole 1? ")
+input("How many putts for hole 2? ")
+input("How many putts for hole 3? ")
+
+print('Good game, ' + name)
+print('Your total score was: ')
